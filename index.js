9bdaeac1db422b09dd760244ae9fe50611feeb95
Thor Kell <thor@tide-pool.ca>
2022-11-05 16:42:28 -0400

    map loading, first marker loading

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 49.397, lng: 150.644 },
-    zoom: 6,
+    center: {lat: 36.8278, lng: -92.7187},
+    zoom: 5,
+  });
+
+  const marker = new google.maps.Marker({
+    position: {lat: 37.344, lng: -100.031},
+    map: map,
   });
 }
 
