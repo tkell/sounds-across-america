89c6c0d098d40ea04c2e0dc2a0b18ee3e26a6eec
Thor Kell <thor@tide-pool.ca>
2023-01-23 19:18:23 -0500

    back to regular YT controls

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function makeInfoWindowHtml(locationName, blurb, youtubeTitle, youtubeId) {
     <div>
       <p class="location-name">${locationName}</p>
       <p class="location-blurb">${blurb}</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}?fs=0&controls=0" title="${youtubeTitle}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}?" title="${youtubeTitle}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
       </iframe>
     </div>
   `
