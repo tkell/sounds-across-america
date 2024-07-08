5d60a4c493fe5a6e47fd137247b5513d1ac11e16
Thor Kell <thor@tide-pool.ca>
2024-07-08 14:13:46 -0400

    fix encoding and stray space in markers.js

--- src/markers.js
+++ src/markers.js
@@ -6,7 +6,7 @@ const markers = [
         },
         "locationName": "Mile 79 - Maine Foghorn",
         "blurb": "This piece features a synth made from the sound of the Portland Head Light in Cape Elizabeth, Maine. Nevermind that it was crystal clear and sunny on the day that I recorded this foghorn. I isolated the tone from the waves in the background, mapped it to a sampler and added a splash of reverb, which led to this creation. ",
-        "youtubeId": "VPkNuea2J64 ",
+        "youtubeId": "VPkNuea2J64",
         "youtubeTitle": "Maine Foghorn",
         "icon": "diamond.svg",
         "releaseDate": "2024-06-01"
@@ -17,7 +17,7 @@ const markers = [
             "lng": -70.6907
         },
         "locationName": "Mile 118 - Maine Crow",
-        "blurb": "The crow was not shy of the microphone. On the second day of my trip, I stopped to record this shrieking crow in southern Maine. I was about halfway into a 93 mile ride from Yarmouth, ME to Stratham, NH. According to a thermometer on a bank in Biddeford, it was 100 degrees that day. I sang Third World’s “96 Degrees In The Shade” to myself as I rode. I turned the crow into an arpeggiated synth with the Microcosm delay pedal and made a beat using mostly sounds recorded on the bike trip. ",
+        "blurb": "The crow was not shy of the microphone. On the second day of my trip, I stopped to record this shrieking crow in southern Maine. I was about halfway into a 93 mile ride from Yarmouth, ME to Stratham, NH. According to a thermometer on a bank in Biddeford, it was 100 degrees that day. I sang Third World's \"96 Degrees In The Shade\" to myself as I rode. I turned the crow into an arpeggiated synth with the Microcosm delay pedal and made a beat using mostly sounds recorded on the bike trip. ",
         "youtubeId": "PeqpPfVFaNQ",
         "youtubeTitle": "Maine Crow",
         "icon": "diamond.svg",
